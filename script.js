ba0ce147a6aeb68823139cb423b8481b3b40fd29
Prazzy <kumaraprasannajayaraju@kumaras-mbp-2.lan>
2024-12-02 23:40:25 -0500

    Added Initial website files

---
+++ script.js
@@ -0,0 +1,7 @@
+// Add interactivity for the architecture section
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("JavaScript Loaded");
+    // Example of interactivity
+    alert("Welcome to the City of Toronto Data Platform!");
+  });
+  
